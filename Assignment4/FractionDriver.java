6e483a56ac813bfadf1138d47971fa67e7f674a0
Error404143 <jordyn.white@hputx.edu>
2025-10-29 22:46:15 -0500

    Update FractionDriver.java
    did the assignment

--- Assignment4/FractionDriver.java
+++ Assignment4/FractionDriver.java
@@ -1,12 +1,53 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class FractionDriver{
-   public static void main(String[] args) throws FileNotFoundException{
-      //TODO: Create a method that will test the add(), subtract(), multiply(), and divide() methods in your Fraction class  
-      //      Also test the constructor method when passing 0 in to the denominator. NOTE: Your driver should not crash because
-      //      you handle the exception properly.
+public class FractionDriver {
+   public static void main(String[] args) throws FileNotFoundException {
+      // Open the file values.txt
+      Scanner fin = new Scanner(new File("values.txt"));
+
+      System.out.println("Reading fractions from file:\n");
+
+      // Read each pair of numbers from file
+      while (fin.hasNextInt()) {
+      int n = fin.nextInt();
+
+      if (!fin.hasNextInt()) {
+         System.out.println("Warning: No denominator found for " + n + ". Skipping...");
+         break;
+      }
+
+      int d = fin.nextInt();
+
+      try {
+         Fraction f = new Fraction(n, d);
+         System.out.println("Original: " + f);
+         f.reduce();
+         System.out.println("Reduced: " + f);
+         System.out.println();
+      } catch (IllegalArgumentException e) {
+         System.out.println("Error creating " + n + "/" + d + ": " + e.getMessage());
+         }
+      }
+
+      fin.close();
+
+      System.out.println("Testing math operations:\n");
+      Fraction f1 = new Fraction(1, 2);
+      Fraction f2 = new Fraction(3, 4);
+
+      System.out.println(f1 + " + " + f2 + " = " + f1.add(f2));
+      System.out.println(f1 + " - " + f2 + " = " + f1.subtract(f2));
+      System.out.println(f1 + " * " + f2 + " = " + f1.multiply(f2));
+      System.out.println(f1 + " / " + f2 + " = " + f1.divide(f2));
+
+      int result = f1.compareTo(f2);
+      if (result < 0)
+         System.out.println(f1 + " is less than " + f2);
+      else if (result > 0)
+         System.out.println(f1 + " is greater than " + f2);
+      else
+         System.out.println(f1 + " is equal to " + f2);
    }
 }
