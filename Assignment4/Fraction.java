2296c67cd01e8736d2cca3cb1fbca4a8f20ed8aa
Error404143 <jordyn.white@hputx.edu>
2025-10-29 22:42:09 -0500

    Update Fraction.java
    did the assignment

--- Assignment4/Fraction.java
+++ Assignment4/Fraction.java
@@ -1,31 +1,82 @@
-/**
-*
-* Using your previous Fraction class, add the following methods to the class
-*  -Fraction add(Fraction f)
-*  -Fraction subtract(Fraction f)
-*  -Fraction multiply(Fraction f)
-*  -Fraction divide(Fraction f)
-*  -int compareTo(Fraction f)
-*
-**/
-public class Fraction{
+public class Fraction {
    private int numerator;
    private int denominator;
-   
-   public Fraction(int n, int d){
-      //TODO: Update this constructor from previous assignments to also throw
-      //      IllegalArgumentException with the message "Cannot have 0 in denominator."
+
+   public Fraction(int n, int d) {
+      if (d == 0) {
+         throw new IllegalArgumentException("Cannot have 0 in denominator.");
+      }
+      numerator = n;
+      denominator = d;
    }
-   
-   public Fraction(int n){
-      //TODO: Finish this constructor so that n is over 1 for a whole number
+
+   public Fraction(int n) {
+      numerator = n;
+      denominator = 1;
    }
-   
-   /*
-     TODO: create the required method so that when 
-     a Fraction object is part of a print() then output
-     is numerator/denominator.
-   */
-   //public...
-      
+
+   public int getNumerator() {
+      return numerator;
+   }
+
+   public int getDenominator() {
+      return denominator;
+   }
+
+   public Fraction add(Fraction f) {
+      int n = (numerator * f.denominator) + (f.numerator * denominator);
+      int d = denominator * f.denominator;
+      return new Fraction(n, d);
+   }
+
+   public Fraction subtract(Fraction f) {
+      int n = (numerator * f.denominator) - (f.numerator * denominator);
+      int d = denominator * f.denominator;
+      return new Fraction(n, d);
+   }
+
+   public Fraction multiply(Fraction f) {
+      int n = numerator * f.numerator;
+      int d = denominator * f.denominator;
+      return new Fraction(n, d);
+   }
+
+   public Fraction divide(Fraction f) {
+      int n = numerator * f.denominator;
+      int d = denominator * f.numerator;
+      return new Fraction(n, d);
+   }
+
+   public int compareTo(Fraction f) {
+      int left = numerator * f.denominator;
+      int right = f.numerator * denominator;
+      if (left < right) {
+         return -1;
+      } else if (left > right) {
+         return 1;
+      } else {
+         return 0;
+      }
+   }
+
+   public void reduce() {
+      int smaller;
+      if (numerator < denominator) {
+         smaller = numerator;
+      } else {
+         smaller = denominator;
+      }
+
+      for (int i = smaller; i > 1; i--) {
+         if (numerator % i == 0 && denominator % i == 0) {
+            numerator = numerator / i;
+            denominator = denominator / i;
+            break; 
+         }
+      }
+   }
+
+   public String toString() {
+      return numerator + "/" + denominator;
+   }
 }
